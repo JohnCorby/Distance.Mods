e7035fbccbadce7f51fa37f94b9c4ecd8caf1df6
JohnCorby <williamforgames6@gmail.com>
2020-12-25 14:13:17 -0800

    bit of refactoring

--- Distance.Raycast/DepthCamera.cs
+++ Distance.Raycast/DepthCamera.cs
@@ -1,52 +1,45 @@
-﻿using System;
-using Reactor.API.Storage;
+﻿using Reactor.API.Storage;
 using UnityEngine;
 
 namespace Distance.Raycast {
     public class DepthCamera : MonoBehaviour {
-        private Camera Camera;
-        private readonly RenderTexture RenderTexture =
-            new(Screen.width / 2, Screen.height / 2, 24);
+        private RenderTexture RenderTexture;
         private Texture2D Texture2D;
+        private Camera Camera;
 
-        private static readonly Material MATERIAL;
+        private static readonly Material DEPTH_MATERIAL;
 
         static DepthCamera() {
-            var assetBundle = (AssetBundle) new Assets("shaders").Bundle;
-            var shader = assetBundle.LoadAsset<Shader>("Depth.shader");
-            MATERIAL = new Material(shader);
+            var assetBundle = (AssetBundle) new Assets("assets").Bundle;
+            DEPTH_MATERIAL = assetBundle.LoadAsset<Material>("Depth.mat");
         }
 
         private void Awake() {
+            RenderTexture = new RenderTexture(256, 256, 0);
+            Texture2D = new Texture2D(RenderTexture.width, RenderTexture.height);
+
             Camera = gameObject.AddComponent<Camera>();
-            // Camera.SetReplacementShader(SHADER, "");
-            // Camera.cullingMask = PhysicsEx.NoCarsRayCastLayerMask_;
+            Camera.cullingMask = PhysicsEx.NoCarsRayCastLayerMask_;
             Camera.depthTextureMode = DepthTextureMode.Depth;
             Camera.targetTexture = RenderTexture;
-
-            Texture2D = new Texture2D(RenderTexture.width, RenderTexture.height);
         }
-
-        // private void OnPreRender() {
-        //     Camera.CopyFrom(Camera.main);
-        // }
 
         /// apply shader to texture
         private void OnRenderImage(RenderTexture src, RenderTexture dest) {
-            Graphics.Blit(src, dest, MATERIAL);
+            Graphics.Blit(src, dest, DEPTH_MATERIAL);
         }
-
-        // /// copy RenderTexture to cpu-readable Texture2D
-        // private void OnPostRender() {
-        //     var lastActive = RenderTexture.active;
-        //     RenderTexture.active = RenderTexture;
-        //     Texture2D.ReadPixels(new Rect(0, 0, Texture2D.width, Texture2D.height), 0, 0);
-        //     RenderTexture.active = lastActive;
-        // }
 
         /// draw texture to screen
         private void OnGUI() {
             GUI.DrawTexture(new Rect(0, 0, RenderTexture.width, RenderTexture.height), RenderTexture);
+        }
+
+        /// copy RenderTexture to cpu-readable Texture2D
+        private void CopyToCpuTexture() {
+            var lastActive = RenderTexture.active;
+            RenderTexture.active = RenderTexture;
+            Texture2D.ReadPixels(new Rect(0, 0, Texture2D.width, Texture2D.height), 0, 0);
+            RenderTexture.active = lastActive;
         }
     }
 }
--- Unity/Assets/Depth.shader
+++ Unity/Assets/Depth.shader
@@ -1,10 +1,13 @@
 ﻿Shader "Custom/Depth" {
     SubShader {
-        Tags { "RenderType"="Opaque" }
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
         Pass {
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
+
             #include "UnityCG.cginc"
 
             struct appdata {
@@ -13,25 +16,25 @@
             };
 
             struct v2f {
-                float4 pos : SV_POSITION;
+                float4 vertex : SV_POSITION;
                 float2 uv : TEXCOORD0;
             };
 
-            sampler2D _CameraDepthTexture;
-
             v2f vert(appdata v) {
                 v2f o;
-                o.pos = UnityObjectToClipPos(v.vertex);
+                o.vertex = UnityObjectToClipPos(v.vertex);
                 o.uv = v.uv;
                 return o;
             }
 
-            half4 frag(v2f i) : SV_Target {
+            sampler2D _CameraDepthTexture;
+
+            fixed4 frag(v2f i) : SV_Target
+            {
                 float depth = tex2D(_CameraDepthTexture, i.uv);
                 depth = Linear01Depth(depth);
-                depth = 1 - depth;
+                depth = 1 - depth; // closer = higher
                 return depth;
-                // return fixed4(depth, depth, depth, 1);
             }
             ENDCG
         }
