04b2d5a245a08f8da60481169740c77603261111
JohnCorby <williamforgames6@gmail.com>
2020-12-25 22:42:17 -0800

    cheats are better and also hotkey enabled

--- Distance.Cheat/Entry.cs
+++ Distance.Cheat/Entry.cs
@@ -12,40 +12,27 @@ namespace Distance.Cheat {
     public class Entry : MonoBehaviour {
         public static readonly Log LOG = LogManager.GetForCurrentAssembly();
 
-        internal static bool IsFreshLevel;
+        public static bool CheatsEnabled;
+
+        [CanBeNull]
+        public static LocalPlayerControlledCar Car;
+        [CanBeNull]
+        public static RaceEndLogic End;
 
         public void Initialize(IManager manager) {
             RuntimePatcher.AutoPatch();
 
-            Events.Level.PostLoad.Subscribe(_ => IsFreshLevel = true);
+            Events.Level.PostLoad.Subscribe(_ => End = FindObjectOfType<RaceEndLogic>());
         }
 
+        private void Awake() {
+            gameObject.AddComponent<Hotkeys>();
+        }
+
+        /// go straight to main menu
         private IEnumerator Start() {
-            // go straight to main menu
             yield return new WaitForEndOfFrame();
             SceneManager.LoadScene("MainMenu");
-        }
-
-        [CanBeNull]
-        internal static RaceEndLogic RaceEndLogic;
-
-        /// when player car spawns for the first time in a level
-        internal static void OnCarSpawn(LocalPlayerControlledCar car) {
-            // update cache
-            RaceEndLogic = FindObjectOfType<RaceEndLogic>();
-
-            Utils.PrintObjects();
-
-            // uh ohhhhh
-            // foreach (var gameObject in FindObjectsOfType<GameObject>()) {
-            //     foreach (var collider in gameObject.GetComponents<Collider>()) {
-            //         collider.enabled = collider.isTrigger;
-            //     }
-            //
-            //     if (gameObject.HasComponentInChildren<Camera>()) continue;
-            //     gameObject.transform.position = Vector3.zero;
-            //     gameObject.transform.rotation.SetLookRotation(Vector3.forward, Vector3.up);
-            // }
         }
     }
 }
---
+++ Distance.Cheat/Hotkeys.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using UnityEngine;
+using static Distance.Cheat.Entry;
+
+namespace Distance.Cheat {
+    public class Hotkeys : MonoBehaviour {
+        static Hotkeys() {
+            Events.Car.Death.SubscribeAll((_, _) => {
+                LOG.Info("CHEATS OFF");
+
+                JetsGadget.thrusterBoostFullPowerLimit_ = 1f;
+                JetsGadget.thrusterBoostDepletedLimit_ = 0.4f;
+            });
+        }
+
+        private void Update() {
+            if (!Input.GetKey(KeyCode.C)) return;
+
+            if (Input.GetKeyDown(KeyCode.E)) { // enable cheats
+                LOG.Info("CHEATS ON");
+
+                if (Car) {
+                    Car.invincible_ = true;
+                    Car.minInstaRespawnTriggerTime_ = 0;
+                    Car.minInstaRespawnTime_ = 0;
+
+                    var boost = Car.GetComponent<BoostGadget>();
+                    boost.accelerationMul_ = 1f * 3;
+                    boost.heatUpRate_ = 0;
+
+                    JetsGadget.thrusterBoostFullPowerLimit_ = 1f * 3;
+                    JetsGadget.thrusterBoostDepletedLimit_ = 0.4f * 3;
+
+                    foreach (var gadget in Car.GetComponents<Gadget>())
+                        gadget.SetAbilityEnabled(true, false);
+
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.D)) { // dump objects
+                var s = new StringBuilder();
+                s.AppendLine("############# BEGIN DumpObjects #############");
+
+                foreach (var gameObject in FindObjectsOfType<GameObject>()) {
+                    s.AppendLine(gameObject.name);
+                    foreach (var component in gameObject.GetComponents<Component>())
+                        s.AppendLine("\t" + component.GetType());
+                }
+
+                s.AppendLine("############# END DumpObjects #############");
+                LOG.Info(s);
+            }
+
+            if (Input.GetKeyDown(KeyCode.End)) { // tp to end
+                if (Car && End)
+                    Car.transform.position = End.transform.position;
+            }
+        }
+    }
+}
