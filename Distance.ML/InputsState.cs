5f2097c3dbfa389f64271124fe3a07486156d694
JohnCorby <williamforgames6@gmail.com>
2021-08-05 23:33:11 -0700

    inputs actually do something now lol

--- Distance.ML/InputsState.cs
+++ Distance.ML/InputsState.cs
@@ -1,30 +1,38 @@
 ﻿using System.Linq;
-using System.Text;
 using UnityEngine;
-using static Distance.ML.Entry;
 
 namespace Distance.ML {
     /// stuff for handling car input
     public class InputsState : MonoBehaviour {
         public uint Actions;
 
-        // private void Awake() {
-        // throw new NotImplementedException();
-        // }
+        private InputStates ComputerStates = null!;
+        private InputStates PlayerStates = null!;
+
+        private void Awake() {
+            ComputerStates = new InputStates();
+            PlayerStates = Utils.PlayerDataLocal!.InputStates_;
+            Utils.PlayerDataLocal.controlProfile_.states_ = ComputerStates;
+        }
 
         /// update state stuff
         public void UpdateState() {
-            var states = Utils.PlayerDataLocal!.InputStates_;
-            // override
-            if (states.states_.Any(state => state.isPressed_)) return;
+            // override if there's player input
+            Utils.PlayerDataLocal!.controlProfile_.states_ =
+                PlayerStates.states_.Any(state => state.isPressed_)
+                    ? PlayerStates
+                    : ComputerStates;
 
-            static void Press(InputState state) {
-                state.isPressed_ = true;
-                state.isTriggered_ = true;
-                state.isReleased_ = false;
-                state.value_ = 1;
+            void Press(InputAction action) {
+                ComputerStates.states_[(int)action] = new InputState {
+                    value_ = 1,
+                    isPressed_ = true,
+                    isTriggered_ = true,
+                    isReleased_ = false
+                };
             }
-            Press(states.GetState(InputAction.Jump));
+
+            Press(InputAction.Jump);
         }
     }
 }
