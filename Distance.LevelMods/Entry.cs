6ddb5b0a97998b4872825d921a069cfbb6e81004
JohnCorby <williamforgames6@gmail.com>
2021-08-07 20:16:01 -0700

    bruh momentum

--- Distance.LevelMods/Entry.cs
+++ Distance.LevelMods/Entry.cs
@@ -1,7 +1,9 @@
-﻿using Reactor.API.Attributes;
+﻿using System.IO;
+using Events.Level;
+using Reactor.API.Attributes;
 using Reactor.API.Interfaces.Systems;
 using Reactor.API.Logging;
-using Reactor.API.Runtime.Patching;
+using Reactor.API.Storage;
 using UnityEngine;
 
 namespace Distance.LevelMods {
@@ -9,6 +11,17 @@ namespace Distance.LevelMods {
     public class Entry : MonoBehaviour {
         public static readonly Log LOG = LogManager.GetForCurrentAssembly();
 
-        public void Initialize(IManager manager) => RuntimePatcher.AutoPatch();
+        public static string ModsFolder = null!;
+
+        public void Initialize(IManager manager) {
+            var fileSystem = new FileSystem();
+            ModsFolder = Path.Combine(fileSystem.RootDirectory, "Mods");
+
+            PostLoad.Subscribe(OnLevelPostLoad);
+        }
+
+        private static void OnLevelPostLoad(PostLoad.Data data) {
+            LOG.Debug($"level {data.level_.Name_} loaded");
+        }
     }
 }
