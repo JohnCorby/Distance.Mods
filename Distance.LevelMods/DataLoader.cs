8cc325578502eef6fd822df261855a22f007e6b5
JohnCorby <williamforgames6@gmail.com>
2021-08-12 22:57:00 -0700

    return component instead of gameobject

--- Distance.LevelMods/DataLoader.cs
+++ Distance.LevelMods/DataLoader.cs
@@ -9,21 +9,23 @@ using static Distance.LevelMods.Entry;
 
 namespace Distance.LevelMods {
     public static class DataLoader {
+        private const string entryName = "Entry";
+
         /// load data as
         /// dll,
         /// bundle with entry script, or
         /// bundle with entry prefab (that has entry script)
-        public static GameObject? Load(byte[] data, string entryName) {
+        public static Component? Load(byte[] data) {
             try {
                 // todo check is url???
 
                 try {
-                    return LoadFromDll(data, entryName);
+                    return LoadFromDll(data);
                 } catch (BadImageFormatException) {
                     log.Info("data is not dll. trying to load as assetbundle");
                 }
 
-                return LoadFromBundle(data, entryName);
+                return LoadFromBundle(data);
             } catch (Exception e) {
                 log.Error($"error loading data: {e.Message}");
                 return null;
@@ -32,17 +34,18 @@ namespace Distance.LevelMods {
 
 
         /// treat data as dll, load entry script
-        private static GameObject LoadFromDll(byte[] data, string entryName) {
+        private static Component LoadFromDll(byte[] data) {
             var dll = Assembly.Load(data);
             var entryTypes = dll.GetExportedTypes().Where(type => type.Name == entryName).ToArray();
 
-            var entryType = GetEntryType(entryTypes, $"dll {dll}", entryName);
-            return new GameObject(entryType.Namespace, entryType);
+            var entryType = GetEntryType(entryTypes, $"dll {dll}");
+            var entryPrefab = new GameObject(entryType.Namespace);
+            return entryPrefab.AddComponent(entryType);
         }
 
         /// treat data as bundle, load dlls, find entry script.
         /// load entry prefab or create new
-        private static GameObject LoadFromBundle(byte[] data, string entryName) {
+        private static Component LoadFromBundle(byte[] data) {
             var bundle = AssetBundle.LoadFromMemory(data)!;
             try {
                 var entryTypes = bundle.GetAllAssetNames()
@@ -52,8 +55,8 @@ namespace Distance.LevelMods {
                     .Where(type => type.Name == entryName)
                     .ToArray();
 
-                var entryType = GetEntryType(entryTypes, $"bundle {bundle.name}", entryName);
-                return GetEntryPrefab(bundle, entryType);
+                var entryType = GetEntryType(entryTypes, $"bundle {bundle.name}");
+                return GetEntryComp(bundle, entryType);
             } catch (Exception) {
                 bundle.Unload(true);
                 throw;
@@ -62,43 +65,42 @@ namespace Distance.LevelMods {
 
 
         /// check entry types, and then check and return single entry type
-        private static Type GetEntryType(IList<Type> entryTypes, string container, string entryName) {
+        private static Type GetEntryType(IList<Type> entryTypes, string container) {
             switch (entryTypes.Count) {
                 case < 1:
-                    throw new InvalidOperationException($"{container} does not have entry type {entryName}");
+                    throw new Exception($"{container} does not have entry type {entryName}");
                 case > 1:
-                    throw new InvalidOperationException($"{container} has multiple entry types " +
-                                                        entryTypes.Join(type => type.FullName));
+                    throw new Exception($"{container} has multiple entry types " +
+                                        entryTypes.Join(type => type.FullName));
             }
 
             var entryType = entryTypes[0];
 
             if (!typeof(SerialComponent).IsAssignableFrom(entryType))
-                throw new InvalidOperationException(
+                throw new Exception(
                     $"entry type {entryType.FullName} in {container} is not a {nameof(SerialComponent)}");
             return entryType;
         }
 
-        /// get entry prefab from bundle
-        /// or create minimal gameobject from entry type
-        private static GameObject GetEntryPrefab(AssetBundle bundle, Type entryType) {
+        /// get entry comp from bundle
+        /// by loading entry prefab
+        /// or making minimal gameobject from entry type
+        private static Component GetEntryComp(AssetBundle bundle, Type entryType) {
             var asset = bundle.GetAllAssetNames()
                 .SingleOrDefault(asset =>
                     asset.EndsWith(".prefab") &&
-                    Path.GetFileNameWithoutExtension(asset) == entryType.Namespace?.ToLower()
-                );
+                    Path.GetFileNameWithoutExtension(asset) == entryType.Namespace?.ToLower());
+            Component entryComp;
             if (asset == null) {
-                log.Info($"no preset found in bundle {bundle.name}. making minimal one from entry type");
-                return new GameObject(entryType.Namespace, entryType);
+                log.Info($"no entry prefab found in bundle {bundle.name}. making minimal one from entry type");
+                var entryPrefab = new GameObject(entryType.Namespace);
+                entryComp = entryPrefab.AddComponent(entryType);
+            } else {
+                var entryPrefab = bundle.LoadAsset<GameObject>(asset);
+                entryComp = entryPrefab.GetComponent(entryType) ??
+                            throw new Exception($"entry prefab {entryPrefab.name} in bundle {bundle.name} " +
+                                                $"does not have entry component {entryType.FullName}");
             }
-
-            var entryPrefab = bundle.LoadAsset<GameObject>(asset);
-
-            var entryComp = entryPrefab.GetComponent(entryType) ??
-                       throw new InvalidOperationException(
-                           $"entry prefab {entryPrefab.name} in bundle {bundle.name} " +
-                           $"does not have entry component {entryType.FullName}"
-                       );
 
             // init bundle field
             {
@@ -108,19 +110,19 @@ namespace Distance.LevelMods {
                     case < 1:
                         log.Warning($"entry type {entryType.FullName} in bundle {bundle.name} " +
                                     "does not has assetbundle field to init");
-                        return entryPrefab;
+                        return entryComp;
                     case > 1:
                         log.Warning($"entry type {entryType.FullName} in bundle {bundle.name} " +
                                     "has multiple assetbundle fields " +
                                     fields.Join(field => field.Name));
-                        return entryPrefab;
+                        return entryComp;
                 }
 
                 var field = fields[0];
                 field.SetValue(entryComp, bundle);
             }
 
-            return entryPrefab;
+            return entryComp;
         }
     }
 }
--- Distance.LevelMods/Proxy.cs
+++ Distance.LevelMods/Proxy.cs
@@ -1,5 +1,7 @@
 using System.IO;
 using System.Windows.Forms;
+using UnityEngine;
+using static Distance.LevelMods.Entry;
 
 namespace Distance.LevelMods {
     /// placeholder for custom objects that will be loaded in
@@ -9,19 +11,34 @@ namespace Distance.LevelMods {
         public override string DisplayName_ => "LevelMods Proxy";
         public override string ComponentDescription_ => "Replaced with custom LevelMods object on load";
 
+        public override void OnLevelEditorEnter() {
+            log.Debug("level editor enter");
+        }
+
         public override void LevelEditorSpawned() {
+            LoadPrefab();
+        }
+
+        public Component? entryComp;
+
+        public override void Visit(IVisitor visitor, ISerializable prefabComp, int version) {
+            var label = new NGUILabelInspector.Label("This is all test stuff lol", Options.warningColor_);
+            visitor.VisitLabel("Warning Label", ref label, true);
+
+            visitor.VisitAction("Load Prefab", LoadPrefab, entryComp == null,
+                Options.Description.Format("Load custom object into this proxy"));
+
+            visitor.VisitReference("Entry Component", entryComp, null);
+        }
+
+        private void LoadPrefab() {
             using var dialog = new OpenFileDialog {
                 Title = "Choose AssetBundle to load"
             };
             if (dialog.ShowDialog() != DialogResult.OK) return;
-            var prefab = DataLoader.Load(File.ReadAllBytes(dialog.FileName), "Entry");
-            Entry.log.Debug($"prefab is {prefab}");
+            entryComp = DataLoader.Load(File.ReadAllBytes(dialog.FileName));
+            log.Debug($"entry component is {entryComp}");
             // Instantiate()
-        }
-
-        public override void Visit(IVisitor visitor, ISerializable prefabComp, int version) {
-            var label = new NGUILabelInspector.Label("You should not be seeing this label", Colors.darkRed);
-            visitor.VisitLabel("WarningLabel", ref label, true);
         }
     }
 }
